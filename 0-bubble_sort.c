4f59115093dc8971ac0e88796ad8757c4d835f9b
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-10-12 09:47:51 +0100

    error 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,6 +2,8 @@
 
 /**
  * bubble_sort - function
+ * @array: member
+ * @size: member
  * Return: Nothing
  */
 
@@ -9,16 +11,15 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
-    	for (i = 0; i < size - 1; i ++)
-    	{
-        	for (j = 0; j < size - i - 1; j++)
-        	{
-            		if (greater(array[j], array[j+1]))
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (greater(array[j], array[j + 1]))
 			{
 				swap(array, j, j + 1);
-            			print_array(array, size);
+				print_array(array, size);
 			}
-        	}
-    	}
-
+		}
+	}
 }
--- 0-main.c
+++ 0-main.c
@@ -14,7 +14,7 @@ int main(void)
 
     print_array(array, n);
     printf("\n");
-    quick_sort(array, n);
+    bubble_sort(array, n);
     printf("\n");
     print_array(array, n);
     return (0);
