4431c755d80d022f301943b19fe37215261210ea
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-10-12 11:03:13 +0100

    error_selection

--- 0-main.c
+++ 0-main.c
@@ -14,7 +14,7 @@ int main(void)
 
     print_array(array, n);
     printf("\n");
-    bubble_sort(array, n);
+    selection_sort(array, n);
     printf("\n");
     print_array(array, n);
     return (0);
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,28 +10,23 @@
 
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int a = 0;
-	int min;
+	size_t i, j, a;
 	int tmp;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		min = array[i];
-		for (j = i; j < size; j++)
+		a = i;
+		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < min)
-			{
-				min = array[j];
+			if (array[j] < array[a])
 				a = j;
-			}
 		}
-	if (min != array[i])
-	{
-		tmp = array[a];
-		array[i] = array[a];
-		array[a] = tmp;
-		print_array(array, size);
-	}
+		if (i != a)
+		{
+			tmp = array[a];
+			array[a] = array[i];
+			array[i] = tmp;
+			print_array(array, size);
+		}
 	}
 }
