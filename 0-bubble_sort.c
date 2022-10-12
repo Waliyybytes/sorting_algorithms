dba8e42c234861e67b24706ef3893e7286b28e87
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-10-12 10:41:27 +0100

    bubble_zerp

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,11 +12,14 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int tmp;
 
+	if (array == NULL || size < 2)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
-			if (greater(array[j], array[j + 1]))
+			if (array[j] > array[j + 1])
 			{
 				tmp = array[j];
 				array[j] = array[j + 1];
