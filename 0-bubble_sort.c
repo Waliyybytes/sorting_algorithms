5a6e298a1fa10c2677660d22211175cd9e0b4440
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-10-12 10:42:58 +0100

    bubble_errors

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -23,7 +23,7 @@ void bubble_sort(int *array, size_t size)
 			{
 				tmp = array[j];
 				array[j] = array[j + 1];
-				array[j + 1] = tmp;		
+				array[j + 1] = tmp;
 				print_array(array, size);
 			}
 		}
